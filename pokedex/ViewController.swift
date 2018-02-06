e739675d89e5f6c0dcc15f327f0b7515f81606bc
Márton Törőcsik <torocsikm91@gmail.com>
2018-02-06 13:52:35 +0100

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Törőcsik Márton on 2018. 02. 06..
+//  Copyright © 2018. Törőcsik Márton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
